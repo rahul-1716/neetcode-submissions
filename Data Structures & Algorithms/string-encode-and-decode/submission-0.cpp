be4369684a9400632c3e78943a0d138d23679ed9
Rahul Nishad <rahulnishad6969@gmail.com>
2026-09-06 21:47:33 +0530

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -2,29 +2,41 @@ class Solution {
 public:
 
     string encode(vector<string>& strs) {
+        if(strs.empty()) return "";
+        vector<int> sizes;
         string res;
-        for(const string& s : strs){
-            res.append(to_string(s.size()));
-            res.push_back('#');
+        for(string& s : strs){
+            sizes.push_back(s.size());
+        }
+        for(int size : sizes){
+            res.append(to_string(size));
+            res.push_back(',');
+        }
+        res.push_back('#');
+        for(string& s : strs){
             res.append(s);
         }
         return res;
     }
 
     vector<string> decode(string s) {
+        if(s.empty()) return {};
+        vector<int> sizes;
         vector<string> res;
         int i = 0;
-        while(i < s.size()){
+        while(s[i] != '#'){
             int j = i;
-            while(s[j] != '#'){
+            while(s[j] != ','){
                 j++;
             }
-            int length = stoi(s.substr(i, j- i));
+            sizes.push_back(stoi(s.substr(i, j - i)));
             i = j + 1;
-            j = i + length;
-            res.push_back(s.substr(i, length));
-            i = j;
         }
-    return res;
+        i++;
+        for(int size : sizes){
+            res.push_back(s.substr(i, size));
+            i += size;
+        }
+            return res;
     }
 };
