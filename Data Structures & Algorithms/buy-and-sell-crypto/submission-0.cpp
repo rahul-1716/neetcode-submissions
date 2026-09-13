f159beee4616f269d336840fa8151265de8b5d88
Rahul Nishad <rahulnishad6969@gmail.com>
2026-09-13 23:00:05 +0530

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int maxP = 0;
-        int minBuy = prices[0];
-
-        for (int& sell : prices) {
-            maxP = max(maxP, sell - minBuy);
-            minBuy = min(minBuy, sell);
+        int minP = prices[0];
+        for(int& sell : prices){
+            maxP = max(maxP, sell - minP);
+            minP = min(minP, sell);
         }
         return maxP;
     }
