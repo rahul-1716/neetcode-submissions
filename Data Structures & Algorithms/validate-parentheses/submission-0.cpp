fd6d86fea76c02f8d9aa55ff337614ed9f7c8766
Rahul Nishad <rahulnishad6969@gmail.com>
2026-09-14 23:17:19 +0530

    Update: validate-parentheses - submission-0

--- Data Structures & Algorithms/validate-parentheses/submission-0.cpp
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         std::stack<char> stack;
-        std::unordered_map<char,char> map;
-        map = {
-            {')','('},
-            {']','['},
-            {'}','{'}
+        std::unordered_map<char,char> hashmap = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['},
         };
-        for(char c : s){
-            if(map.count(c)){
-                if(!stack.empty() && stack.top() == map[c]){
+        for(char C : s){
+            if(hashmap.count(C)){
+                if(!stack.empty() && stack.top() == hashmap[C]){
                     stack.pop();
                 }else{
                     return false;
                 }
             }else{
-                stack.push(c);
+                stack.push(C);
             }
         }
         return stack.empty();
