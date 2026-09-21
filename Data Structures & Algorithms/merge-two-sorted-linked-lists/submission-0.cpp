1247212b7b7de3f6502364d1b781a86b639aeb30
Rahul Nishad <rahulnishad6969@gmail.com>
2026-09-21 23:56:21 +0530

    Update: merge-two-sorted-linked-lists - submission-0

--- Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.cpp
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.cpp
@@ -12,18 +12,26 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1) {
-            return list2;
+        ListNode dummy(0);
+        ListNode* node = &dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                node->next = list1;
+                list1 = list1->next;
+            } else {
+                node->next = list2;
+                list2 = list2->next;
+            }
+            node = node->next;
         }
-        if (!list2) {
-            return list1;
-        }
-        if (list1->val <= list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+
+        if (list1) {
+            node->next = list1;
         } else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+            node->next = list2;
         }
+
+        return dummy.next;
     }
 };
