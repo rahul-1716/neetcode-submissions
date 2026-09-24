16fe568a3145712b3eee3902ceeb15314bd19cf4
Rahul Nishad <rahulnishad6969@gmail.com>
2026-09-24 23:37:33 +0530

    Update: number-of-one-bits - submission-0

--- Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int res = 0;
-        for(int i = 0; i < 32; i++){
-            if((1 << i) & n){
-                res++;
-            }
+        while (n) {
+            n &= n - 1;
+            res++;
         }
         return res;
     }
